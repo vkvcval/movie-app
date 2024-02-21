0d40dad4354eee4fb490b19cbe6acd474778e747
Valentina Vukovic <valentininaemailadresa@gmail.com>
2024-02-21 13:57:26 +0100

    modify NavBar component

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,23 +1,21 @@
-import Link from "next/link";
-import Logo from "../logo";
-import SearchMovies from "@/components/search";
+import Link from 'next/link'
+import Logo from '../logo'
+import SearchMovies from '@/components/search'
 
 export default function NavBar() {
   return (
-    <nav className="bg-primary drop-shadow-glow shadow-2xl z-50 sticky top-0">
-      <div className="flex place-content-between items-center max-w-screen-xl m-auto">
+    <nav className="sticky top-0 z-50 bg-primary shadow-2xl drop-shadow-glow">
+      <div className="m-auto flex max-w-screen-xl place-content-between items-center">
         <div className="">
           <Link href="/discover">
-            <div className="container mx-auto flex gap-3 items-end p-2">
-              <Logo color="#ccd435" className="w-20 h-20" />
-              <span className="text-secondary text-[44px] font-rowdies ">
-                Movie finder
-              </span>
+            <div className="container mx-auto flex items-end gap-3 p-2">
+              <Logo color="#ccd435" className="h-20 w-20" />
+              <span className="font-rowdies text-[44px] text-secondary ">Movie finder</span>
             </div>
           </Link>
         </div>
         <SearchMovies />
       </div>
     </nav>
-  );
+  )
 }
