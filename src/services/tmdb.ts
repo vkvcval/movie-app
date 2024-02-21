a072ada32324cb21f2bedea428b2ea01d650348d
Valentina Vukovic <valentininaemailadresa@gmail.com>
2024-02-21 13:56:50 +0100

    modify tmdb services

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -1,23 +1,34 @@
 "use server";
 
-import { Configuration, Movie, PopularMovies, Search } from "tmdb-ts";
+import { Configuration, ErrorResponse, Genre, Movie, PopularMovies, ProductionCompany, ProductionCountry, Search } from "tmdb-ts";
 import { tmdbBaseUrl, tmdbConfigurationUrl } from "@/lib/tmdb";
-import { notFound } from "next/navigation";
 import { Genres } from "tmdb-ts/dist/endpoints";
 
-export const fetchData = async <T>(url: string): Promise<T | null> => {
+export const fetchData = async <T>(url: string): Promise<{ data: T, error: ErrorResponse | null }> => {
+  let error = null;
+  let data = null;
+
   try {
     const res = await fetch(url);
-    const data = await res.json();
+    const resJson = await res.json();
 
-    if (!data) {
-      notFound();
+    if (resJson.success === false) {
+      error = resJson
+    } else {
+      data = resJson
     }
-    return data;
-  } catch (error) {
-    console.log(error);
-    return null;
+
+  } catch (e) {
+    console.error(e)
+
+    error = {
+      success: false,
+      status_code: 500,
+      status_message: e instanceof Error ? e.message : "Internal Server Error"
+    }
   }
+
+  return { error, data }
 };
 
 export const getConfiguration = async () => {
@@ -57,8 +68,26 @@ export const getMovieRecommendations = async (movieId: string | number) => {
 
 export const getMovieById = async (movieId: string | number) => {
   const url = `${tmdbBaseUrl}/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`;
-  return fetchData(url);
+  return fetchData<Movie & {
+    genres: Genre[], homepage: string, imdb_id: string; status: String; production_companies: ProductionCompany[], production_countries: ProductionCountry[]
+  }>(url);
 };
+
+export const getGenres = async () => {
+  const url = `${tmdbBaseUrl}/genre/movie/list?api_key=${process.env.TMDB_API_KEY}`;
+  return fetchData<Genres>(url);
+};
+
+export const searchMovie = async (term: string) => {
+  const url = `${tmdbBaseUrl}/search/movie?query=${term}&api_key=${process.env.TMDB_API_KEY}`;
+  return fetchData<Search<Movie>>(url);
+};
+
+export const searchMoviesByGenre = async (genreIds: string[]) => {
+  const url = `${tmdbBaseUrl}/discover/movie?with_genres=${genreIds.join(',')}&api_key=${process.env.TMDB_API_KEY}`;
+  return fetchData<Search<Movie>>(url);
+};
+
 
 export const getMovieImages = async (movieId: string | number) => {
   const url = `${tmdbBaseUrl}/movie/${movieId}/images?api_key=${process.env.TMDB_API_KEY}`;
@@ -79,20 +108,3 @@ export const getMovieVideos = async (movieId: string | number) => {
   const url = `${tmdbBaseUrl}/movie/${movieId}/videos?api_key=${process.env.TMDB_API_KEY}`;
   return fetchData(url);
 };
-
-export const getGenres = async () => {
-  const url = `${tmdbBaseUrl}/genre/movie/list?api_key=${process.env.TMDB_API_KEY}`;
-  return fetchData<Genres>(url);
-};
-
-export const searchMovie = async (term: string) => {
-  const url = `${tmdbBaseUrl}/search/movie?query=${term}&api_key=${process.env.TMDB_API_KEY}`;
-  return fetchData<Search<Movie>>(url);
-};
-
-export const searchMoviesByGenre = async (genreIds: string[]) => {
-  const url = `${tmdbBaseUrl}/discover/movie?with_genres=${genreIds.join(',')}&api_key=${process.env.TMDB_API_KEY}`;
-  return fetchData<Search<Movie>>(url);
-};
-
-
