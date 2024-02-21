4f451c0bf3bf796b18e725ee6bd5a202aed11ae4
Valentina Vukovic <valentininaemailadresa@gmail.com>
2024-02-21 03:30:12 +0100

    modify card component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,26 +1,20 @@
-import { getPosterPath } from "@/lib/tmdb";
-import ArrowTrendingUpIcon from "@/components/icons/ArrowTrendingUpIcon";
+import { getPosterPath } from '@/lib/tmdb'
+import { ArrowTrendingUpIcon } from '@heroicons/react/24/outline'
 
 interface Props {
-  title: string;
-  image: string;
-  popularity: number;
+  title: string
+  image: string
+  popularity: number
 }
 
 export default function Card({ title, image, popularity }: Props) {
   return (
     <div className="relative shadow-md">
-      <img
-        src={getPosterPath(image)}
-        alt={title}
-        className="object-contain h-96"
-      />
-      <div className="absolute z-20 bottom-0 flex items-center gap-1">
-        <ArrowTrendingUpIcon color="green" className="w-10 h-10" />
-        <span className="text-white font-bold text-2xl bg-stone-800">
-          {popularity}
-        </span>
+      <img src={getPosterPath(image)} alt={title} className="h-96 object-contain" />
+      <div className="absolute bottom-0 z-20 flex items-center   gap-1">
+        <ArrowTrendingUpIcon color="green" className="h-10 w-10" />
+        <span className="text-2xl bg-stone-800 font-bold text-white">{popularity}</span>
       </div>
     </div>
-  );
+  )
 }
